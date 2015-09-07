95b2dba8a00194f33a58d5c0691f81e7b733e586
Courtneyjones <me@courtneyejones.com>
2015-09-06 20:44:17 -0400

    deleting 2 optional fog lines from the carrierwave initializer

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,8 +4,6 @@ CarrierWave.configure do |config|
     :aws_access_key_id      => ENV['AWS_ID'],                        # required
     :aws_secret_access_key  => ENV['AWS_SECRET'],                        # required
     :region                 => 'us-east-1',                  # optional, defaults to 'us-east-1'
-    :host                   => 's3.example.com',             # optional, defaults to nil
-    :endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
   }
   config.fog_directory  = 'courtneyandkevin'                     # required
   config.fog_public     = false                                   # optional, defaults to true
