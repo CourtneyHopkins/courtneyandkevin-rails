c3d8b2177b8cd7a26466b06829f055d1aa093c4a
Kevin Hopkins <kevinh@generalassemb.ly>
2015-10-14 00:46:59 -0400

    moved uploaded assets to use env in path

--- app/uploaders/gallery_featured_image_uploader.rb
+++ app/uploaders/gallery_featured_image_uploader.rb
@@ -1,14 +1,14 @@
 class GalleryFeaturedImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
-  
+
   if Rails.env.development? || Rails.env.test?
     storage :file
-  else 
+  else
     storage :fog
   end
 
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads/#{Rails.env}/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   process resize_to_fill: [600, 600]
--- app/uploaders/post_image_uploader.rb
+++ app/uploaders/post_image_uploader.rb
@@ -1,13 +1,13 @@
 class PostImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
-  
+
   if Rails.env.development? || Rails.env.test?
     storage :file
-  else 
+  else
     storage :fog
   end
 
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads/#{Rails.env}/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 end
